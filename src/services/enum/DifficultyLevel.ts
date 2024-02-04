3e551a0c8da024714ec0c1e8e538b52c0150e958
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-02-04 10:41:30 +0100

    initial commit

---
+++ src/services/enum/DifficultyLevel.ts
@@ -0,0 +1,11 @@
+/**
+ * Difficulty Level
+ */
+enum DifficultyLevel {
+  VERY_EASY = 1,
+  EASY = 2,
+  NORMAL = 3,
+  HARD = 4,
+  VERY_HARD = 5
+}
+export default DifficultyLevel
---
+++ src/store/state.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import { name } from '@/../package.json'
+import DifficultyLevel from '@/services/enum/DifficultyLevel'
+
+export const useStateStore = defineStore(`${name}.state`, {
+  state: () => {
+    return {
+      language: 'en',
+      baseFontSize: 1.0,
+      setup: {
+        difficultyLevel: DifficultyLevel.NORMAL
+      },
+      rounds: []
+    } as State
+  },
+  actions: {
+    resetGame() {
+      this.rounds = []
+    },
+    storeRound(round : Round) {
+      this.rounds = this.rounds.filter(item => item.round < round.round)
+      this.rounds.push(round)
+    }
+  },
+  persist: true
+})
+
+export interface State {
+  language: string,
+  baseFontSize: number,
+  setup: Setup,
+  rounds: Round[]
+}
+export interface Setup {
+  difficultyLevel: DifficultyLevel
+  debugMode?: boolean
+}
+
+export interface Round {
+  round: number
+}
